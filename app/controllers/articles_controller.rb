553aad6f90fd33fd4dd034f201b4a4d8d99536e6
Meruyert <meruyert.zhuvandykova@nu.edu.kz>
2017-06-28 06:57:25 +0000

    change gemfile

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
     def index
         @article = Article.all
     end
@@ -9,15 +10,41 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
+            flash[:notice] = "Good boy! or girl, dont care"
             redirect_to articles_path
         else
           render "new"  
         end
     end
     
+    def show
+       
+    end    
+    def edit 
+       
+    end
+    def update
+       
+       if @article.update(article_params)
+           flash[:notice] = "Successfully edited"
+           redirect_to article_path(@article)
+        else
+            render "edit"
+       end
+    end
+    def destroy 
+       
+       @article.destroy
+       flash[:notice] = "MONSTER! YOU KILLED IT!"
+       redirect_to articles_path
+    end
     private
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+    
+    def set_article
+        @article = Article.find(params[:id])
     end
 
 end
